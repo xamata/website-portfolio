31a987f020ce67cb0762a0c697fb251db1688593
xamata <61128570+xamata@users.noreply.github.com>
2024-07-31 21:11:51 -0700

    API URL within Enviornment.ts

--- src/app/email.service.ts
+++ src/app/email.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmailService {
-  private apiUrl = 'testingapi.com';
+  private apiUrl = environment.API_URL;
 
   constructor(private http: HttpClient) {}
 
